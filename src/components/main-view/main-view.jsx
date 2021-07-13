0ce01e8bde101883d840cfbdbc5a88e94374ab24
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-07-13 18:37:57 +0200

    change execution order within component main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,15 +11,16 @@ class MainView extends React.Component {
         { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
         { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
         { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
-      ]
+      ],
+      selectedMovie: null
     }
   }
   render() {
     const { movies, selectedMovie } = this.state;
 
-    if (selectedMovie) return <MovieView movie={selectedMovie} />;
-
     if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
+    if (selectedMovie) return <MovieView movie={selectedMovie} />;
   
     return (
       <div className="main-view">
