704b31a3eb91f2b8707dfde783f7a9e8f4145c07
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-07-22 11:18:28 +0200

    fix bug with button to divert to movie-view

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -10,11 +10,11 @@ export class MovieCard extends React.Component {
     const { movieData, onMovieClick } = this.props;
     return (
       <Card>
-        <Card.Img variant="top" class="mx-auto img-movie" src={movieData.imagePath} />
-        <Card.Body class="movie-card text-center">
+        <Card.Img variant="top" Class="mx-auto img-movie" src={movieData.imagePath} />
+        <Card.Body Class="movie-card text-center">
           <Card.Title>{movieData.Title}</Card.Title>
           <Card.Text>{movieData.Description}</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+          <Button onClick={() => onMovieClick(movieData)} variant="link">Open</Button>
         </Card.Body>
       </Card>
     );
